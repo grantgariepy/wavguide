e67fc32e6bbb00521238c11fdd240224c050472e
grantgariepy <grantgariepy@protonmail.com>
2023-03-07 16:10:03 -0700

    search not working on search page

--- frontend/src/routes/search/[searchTerm]/+page.server.ts
+++ frontend/src/routes/search/[searchTerm]/+page.server.ts
@@ -3,14 +3,17 @@ import type { ServerLoad } from '@sveltejs/kit';
 
 export const load: ServerLoad = async ({ params }) => {
 	const searchTerm: string | undefined = params.searchTerm;
+
 	const fetchSearchResults = async (searchTerm: any) => {
 		const response = await getSearchResults(searchTerm);
-		const { albums } = await response.json();
+		const results = await response.json();
 		// console.log(albums.items);
-		return albums.items;
+		// console.log(results.albums.items);
+		return results;
 	};
+	let promise = fetchSearchResults(searchTerm);
 	return {
-		items: fetchSearchResults(searchTerm)
+		results: fetchSearchResults(searchTerm)
 	};
 	// console.log(response.albums.items);
 };
--- frontend/src/routes/search/[searchTerm]/+page.svelte
+++ frontend/src/routes/search/[searchTerm]/+page.svelte
@@ -1,17 +1,14 @@
 <script lang="ts">
 	export let data: any;
 
-	// console.log(data);
-
-	const { items } = data;
-	// $: results = {items}
+	const { items } = data.results.albums;
+	// console.log(items);
 	const albums = items.slice(0, 10).map((album: any) => ({
 		artist: album.artists.map((_artist: any) => _artist.name).join(', '),
 		songUrl: album.external_urls.spotify,
 		title: album.name,
 		coverImage: album.images[2].url
 	}));
-	// console.log(items);
 </script>
 
 <div class="mx-auto max-w-7xl sm:px-6 h-full px-2">
@@ -19,18 +16,16 @@
 		<p class="text-white font-bold text-xl" id="logo">Search Results</p>
 	</div>
 	<ul class="list-none">
-		{#key albums.title}
-			{#each albums as album}
-				<li class="flex flex-row justify-between gap-4 p-4 hover:bg-secondary hover:cursor-pointer">
-					<img src={album.coverImage} alt="{album.title}-{album.artist}" />
-					<div class="flex items-center justify-center">
-						<p class="text-2xl text-white ">
-							{album.title} - {album.artist}
-						</p>
-					</div>
-					<a href={album.songUrl} class="btn btn-info pull-right">Listen on Spotify</a>
-				</li>
-			{/each}
-		{/key}
+		{#each albums as album}
+			<li class="flex flex-row justify-between gap-4 p-4 hover:bg-secondary hover:cursor-pointer">
+				<img src={album.coverImage} alt="{album.title}-{album.artist}" />
+				<div class="flex items-center justify-center">
+					<p class="text-2xl text-white ">
+						{album.title} - {album.artist}
+					</p>
+				</div>
+				<a href={album.songUrl} class="btn btn-info pull-right">Listen on Spotify</a>
+			</li>
+		{/each}
 	</ul>
 </div>
