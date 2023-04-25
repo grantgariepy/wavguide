d63c0d40844c127acba225bda72b0265509f7dd9
grantgariepy <grantgariepy@protonmail.com>
2023-04-25 15:47:38 -0600

    hero stuff

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -26,7 +26,7 @@
 			<!-- <img src="/images/waveform.jpg" alt="" class="z-0 md:w-max md:h-max object-cover absolute" /> -->
 			<!-- image - end -->
 			<div
-				class="max-w-screen md:text-[14rem] text-6xl w-full flex flex-col justify-center items-center"
+				class="max-w-2xl md:text-[14rem] text-6xl w-full flex flex-col justify-center items-center"
 			>
 				<h1 id="logo4" class=" logo absolute top-96  text-blue-500 font-bold ">WAVGUIDE</h1>
 				<h1 id="logo2" class="logo absolute top-96 text-yellow-300 font-bold  ">WAVGUIDE</h1>
@@ -96,19 +96,36 @@
 	@keyframes slide2 {
 		to {
 			transform: translateY(-100%);
-			transform: scale(150%);
+			transform: scale(1.5);
 		}
 	}
 	@keyframes slide3 {
 		to {
 			transform: translateY(-200%);
-			transform: scale(125%);
+			transform: scale(1.25);
 		}
 	}
 	@keyframes slide4 {
 		to {
 			transform: translateY(-350%);
-			transform: scale(175%);
+			transform: scale(1.75);
+		}
+	}
+	@media only screen and (max-width: 600px) {
+		@keyframes slide2 {
+			to {
+				transform: translateY(200px);
+			}
+		}
+		@keyframes slide3 {
+			to {
+				transform: translateY(400px);
+			}
+		}
+		@keyframes slide4 {
+			to {
+				transform: translateY(600px);
+			}
 		}
 	}
 </style>
